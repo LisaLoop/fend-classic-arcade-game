3caa4f9ce6bff8ce10c29983151a5d95ec112d3f
Lisa Lehrman <lisa.lehrman@gmail.com>
2018-05-23 18:28:53 -0700

    update function moves an enemy across screen

--- js/app.js
+++ js/app.js
@@ -2,8 +2,7 @@
 var Enemy = function() {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
-    var enemyBug1 = new Enemy;
-    var enemyBug2 = new Enemy;
+    
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
@@ -15,6 +14,8 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+    this.x += this.speed * dt;
+    this.speed = 50.0;
 };
 
 // Draw the enemy on the screen, required method for game
@@ -27,12 +28,15 @@ Enemy.prototype.render = function() {
 // a handleInput() method.
 var Player = function() {
     this.sprite = 'images/char-cat-girl.png';
+    
 }
 
 Player.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+    this.x += this.speed * dt;
+    // this.speed = 100;
 };
 
 Player.prototype.render = function() {
@@ -45,13 +49,25 @@ Player.prototype.handleInput = function() {
 
 
 // Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
-var allEnemies = [ enemyBug1, enemyBug2
-    
-];
-var player = new Player;
+var enemyBug1 = new Enemy;
+    enemyBug1.x = 0;
+    enemyBug1.y = 60;
+    enemyBug1.speed = 10.0;
 
+
+var enemyBug2 = new Enemy;
+    enemyBug2.x = 200;
+    enemyBug2.y = 200;
+    enemyBug1.speed = 3.0;
+
+// Place all enemy objects in an array called allEnemies
+var allEnemies = [ enemyBug1, enemyBug2   
+];
+// Place the player object in a variable called player
+var player = new Player;
+    player.x = 200;
+    player.y = 400;
+    // player.speed = 1.0;
 
 
 // This listens for key presses and sends the keys to your
@@ -63,6 +79,7 @@ document.addEventListener('keyup', function(e) {
         39: 'right',
         40: 'down'
     };
-
     player.handleInput(allowedKeys[e.keyCode]);
 });
+
+
